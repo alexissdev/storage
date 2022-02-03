2b84e6c07a79b77fc583b56b27bcb35824564a04
pixeldev <pixeldev1729@gmail.com>
2022-02-02 18:37:28 -0600

    build: add maven publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,28 @@ plugins {
 
 subprojects {
     apply(plugin="java-library")
+    apply(plugin="maven-publish")
+
+    tasks {
+        java {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(8))
+            }
+        }
+
+        compileJava {
+            options.compilerArgs.add("-parameters")
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                artifactId = "storage-${project.name}"
+                from(components["java"])
+            }
+        }
+    }
 
     repositories {
         mavenLocal()
