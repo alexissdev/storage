9e5293be0258340e012933cb69ac6984ad5e83ef
pixeldev <pixeldev1729@gmail.com>
2022-02-05 00:02:05 -0600

    refactor(mongo-dist): use RemoteModelService

--- mongo-dist/src/main/java/net/cosmogrp/storage/MongoModelService.java
+++ mongo-dist/src/main/java/net/cosmogrp/storage/MongoModelService.java
@@ -2,123 +2,32 @@ package net.cosmogrp.storage;
 
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.ReplaceOptions;
-import net.cosmogrp.storage.dist.LocalModelService;
+import net.cosmogrp.storage.dist.RemoteModelService;
 import net.cosmogrp.storage.model.Model;
-import net.cosmogrp.storage.model.meta.Cached;
 import net.cosmogrp.storage.model.meta.ModelMeta;
 import org.jetbrains.annotations.Nullable;
 import org.mongojack.JacksonMongoCollection;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
-import java.util.function.Consumer;
 
 public class MongoModelService<T extends Model>
-        extends AsyncModelService<T> {
+        extends RemoteModelService<T> {
 
     private final JacksonMongoCollection<T> collection;
-    private final ModelService<T> cacheModelService;
 
     public MongoModelService(
             Executor executor,
             ModelMeta<T> modelMeta,
             JacksonMongoCollection<T> collection
     ) {
-        super(executor);
+        super(executor, modelMeta);
         this.collection = collection;
-
-        Cached.Strategy strategy = modelMeta.getCachedStrategy();
-
-        // same models cannot be cached
-        if (strategy == null) {
-            this.cacheModelService = null;
-        } else {
-            if (strategy == Cached.Strategy.LOCAL) {
-                this.cacheModelService = new LocalModelService<>();
-            } else {
-                this.cacheModelService = null;
-            }
-        }
     }
 
     @Override
-    public @Nullable T findSync(String id) {
-        T model = collection.findOneById(id);
-
-        if (model != null) {
-            // cache model if possible
-            if (cacheModelService != null) {
-                cacheModelService.saveSync(model);
-            }
-        }
-
-        return model;
-    }
-
-    @Override
-    public @Nullable T getSync(String id) {
-        if (cacheModelService == null) {
-            return null;
-        }
-
-        return cacheModelService.getSync(id);
-    }
-
-    @Override
-    public @Nullable T getOrFindSync(String id) {
-        T model = getSync(id);
-
-        if (model != null) {
-            return model;
-        }
-
-        return findSync(id);
-    }
-
-    @Override
-    public List<T> getAllSync() {
-        if (cacheModelService == null) {
-            return Collections.emptyList();
-        } else {
-            return cacheModelService.getAllSync();
-        }
-    }
-
-    @Override
-    public List<T> findAllSync() {
-        List<T> loadedModels = collection.find()
-                .into(new ArrayList<>());
-
-        if (cacheModelService != null) {
-            for (T model : loadedModels) {
-                cacheModelService.saveSync(model);
-            }
-        }
-
-        return loadedModels;
-    }
-
-    @Override
-    public void saveSync(T model) {
-        if (cacheModelService != null) {
-            cacheModelService.saveSync(model);
-        }
-
-        internalSave(model);
-    }
-
-    @Override
-    public void uploadSync(T model) {
-        if (cacheModelService != null) {
-            cacheModelService.deleteSync(model);
-        }
-
-        internalSave(model);
-    }
-
-    private void internalSave(T model) {
+    protected void internalSave(T model) {
         collection.replaceOne(
                 Filters.eq("_id", model.getId()),
                 model, new ReplaceOptions().upsert(true)
@@ -126,30 +35,14 @@ public class MongoModelService<T extends Model>
     }
 
     @Override
-    public void uploadAllSync(Consumer<T> preUploadAction) {
-        if (cacheModelService != null) {
-            List<T> models = cacheModelService.getAllSync();
-            for (T model : models) {
-                preUploadAction.accept(model);
-                uploadSync(model);
-            }
-        }
+    protected @Nullable T internalFind(String id) {
+        return collection.findOneById(id);
     }
 
     @Override
-    public void deleteSync(T model) {
-        model.delete();
-        uploadSync(model);
+    protected List<T> internalFindAll() {
+        return collection.find()
+                .into(new ArrayList<>());
     }
 
-    @Override
-    public T deleteSync(String id) {
-        T model = getOrFindSync(id);
-
-        if (model != null) {
-            deleteSync(model);
-        }
-
-        return model;
-    }
 }
