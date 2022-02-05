dcf83cef164d1edceaf1dd1be195c9e656c5ad46
pixeldev <pixeldev1729@gmail.com>
2022-02-05 09:42:25 -0600

    refactor(api): remove cache strategy

--- api/src/main/java/net/cosmogrp/storage/model/meta/ModelMeta.java
+++ api/src/main/java/net/cosmogrp/storage/model/meta/ModelMeta.java
@@ -1,9 +1,6 @@
 package net.cosmogrp.storage.model.meta;
 
-import net.cosmogrp.storage.model.AnnotationHelper;
 import net.cosmogrp.storage.model.Model;
-import net.cosmogrp.storage.model.exception.NoSuchModelDataException;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Responsible for storing metadata about a model.
@@ -17,32 +14,13 @@ import org.jetbrains.annotations.Nullable;
 public class ModelMeta<T extends Model> {
 
     private final Class<T> type;
-    private final Cached.Strategy cachedStrategy;
 
-    public ModelMeta(Class<T> type) throws NoSuchModelDataException {
+    public ModelMeta(Class<T> type) {
         this.type = type;
-
-        Cached cached = AnnotationHelper.getAnnotation(
-                type, Cached.class, false
-        );
-
-        if (cached != null) {
-            cachedStrategy = cached.value();
-        } else {
-            cachedStrategy = null;
-        }
     }
 
     public Class<T> getType() {
         return type;
     }
 
-    /**
-     * @return the cached strategy for this model
-     * NOTE: If no cached strategy is defined, so
-     * it might not be possible to cache the model.
-     */
-    public @Nullable Cached.Strategy getCachedStrategy() {
-        return cachedStrategy;
-    }
 }
