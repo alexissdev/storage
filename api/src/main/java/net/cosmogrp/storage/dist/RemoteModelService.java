9a5cd23aa682987c423c81c39aad8ce5d772e27d
pixeldev <pixeldev1729@gmail.com>
2022-02-07 10:07:20 -0600

    refactor(api): don't force to use a cached service

--- api/src/main/java/net/cosmogrp/storage/dist/RemoteModelService.java
+++ api/src/main/java/net/cosmogrp/storage/dist/RemoteModelService.java
@@ -1,122 +1,67 @@
 package net.cosmogrp.storage.dist;
 
 import net.cosmogrp.storage.AsyncModelService;
-import net.cosmogrp.storage.ModelService;
 import net.cosmogrp.storage.model.Model;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 
 public abstract class RemoteModelService<T extends Model>
         extends AsyncModelService<T> {
-
-    protected final ModelService<T> cacheModelService;
-
-    public RemoteModelService(
-            Executor executor,
-            ModelService<T> cacheModelService
-    ) {
+    public RemoteModelService(Executor executor) {
         super(executor);
-        this.cacheModelService = cacheModelService;
     }
 
     @Override
     public @Nullable T findSync(String id) {
-        T model = internalFind(id);
-
-        if (model != null) {
-            // cache model if possible
-            if (cacheModelService != null) {
-                cacheModelService.saveSync(model);
-            }
-        }
-
-        return model;
+        return internalFind(id);
     }
 
     @Override
     public @Nullable T getSync(String id) {
-        if (cacheModelService == null) {
-            return null;
-        }
-
-        return cacheModelService.getSync(id);
+        return findSync(id);
     }
 
     @Override
     public @Nullable T getOrFindSync(String id) {
-        T model = getSync(id);
-
-        if (model != null) {
-            return model;
-        }
-
         return findSync(id);
     }
 
     @Override
     public List<T> getAllSync() {
-        if (cacheModelService == null) {
-            return Collections.emptyList();
-        } else {
-            return cacheModelService.getAllSync();
-        }
+        return internalFindAll();
     }
 
     @Override
     public List<T> findAllSync() {
-        List<T> loadedModels = internalFindAll();
-
-        if (cacheModelService != null) {
-            for (T model : loadedModels) {
-                cacheModelService.saveSync(model);
-            }
-        }
-
-        return loadedModels;
+        return internalFindAll();
     }
 
     @Override
     public void saveSync(T model) {
-        if (cacheModelService != null) {
-            cacheModelService.saveSync(model);
-        }
-
         internalSave(model);
     }
 
     @Override
     public void uploadSync(T model) {
-        if (cacheModelService != null) {
-            cacheModelService.deleteSync(model);
-        }
-
         internalSave(model);
     }
 
     @Override
     public void uploadAllSync(Consumer<T> preUploadAction) {
-        if (cacheModelService != null) {
-            List<T> models = cacheModelService.getAllSync();
-            for (T model : models) {
-                preUploadAction.accept(model);
-                uploadSync(model);
-            }
-        }
+        // do nothing
     }
 
     @Override
     public void deleteSync(T model) {
-        model.delete();
-        uploadSync(model);
+        internalDelete(model);
     }
 
     @Override
     public T deleteSync(String id) {
-        T model = getOrFindSync(id);
+        T model = findSync(id);
 
         if (model != null) {
             deleteSync(model);
@@ -127,8 +72,9 @@ public abstract class RemoteModelService<T extends Model>
 
     protected abstract void internalSave(T model);
 
+    protected abstract void internalDelete(T model);
+
     protected abstract @Nullable T internalFind(String id);
 
     protected abstract List<T> internalFindAll();
-
 }
