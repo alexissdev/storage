18e7253c56528bb1ec9e74d2db3964c5ff39b29f
pixeldev <pixeldev1729@gmail.com>
2022-02-07 10:08:01 -0600

    refactor(redis-dist): don't assume that always needs to be cached

---
+++ redis-dist/src/main/java/net/cosmogrp/storage/redis/AbstractRedisModelService.java
@@ -0,0 +1,69 @@
+package net.cosmogrp.storage.redis;
+
+import com.google.gson.Gson;
+import net.cosmogrp.storage.dist.RemoteModelService;
+import net.cosmogrp.storage.model.Model;
+import net.cosmogrp.storage.model.meta.ModelMeta;
+import net.cosmogrp.storage.redis.connection.RedisCache;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+public class AbstractRedisModelService<T extends Model>
+        extends RemoteModelService<T> {
+    private final Gson gson;
+    private final Class<T> type;
+    private final RedisCache redisCache;
+    private final String tableName;
+
+    public AbstractRedisModelService(
+            Executor executor,
+            ModelMeta<T> modelMeta,
+            Gson gson,
+            RedisCache redisCache
+    ) {
+        super(executor);
+        this.gson = gson;
+        this.type = modelMeta.getType();
+        this.redisCache = redisCache;
+        this.tableName = (String) modelMeta.getProperty("redis-table");
+    }
+
+    @Override
+    protected void internalSave(T model) {
+        redisCache.set(
+                tableName, model.getId(),
+                gson.toJson(model)
+        );
+    }
+
+    @Override
+    protected void internalDelete(T model) {
+        redisCache.del(tableName, model.getId());
+    }
+
+    @Override
+    protected @Nullable T internalFind(String id) {
+        String json = redisCache.get(tableName, id);
+
+        if (json == null) {
+            return null;
+        }
+
+        return gson.fromJson(json, type);
+    }
+
+    @Override
+    protected List<T> internalFindAll() {
+        List<String> values = redisCache.getAllValues(tableName);
+        List<T> models = new ArrayList<>();
+
+        for (String value : values) {
+            models.add(gson.fromJson(value, type));
+        }
+
+        return models;
+    }
+}
--- redis-dist/src/main/java/net/cosmogrp/storage/redis/CachedRedisModelService.java
+++ redis-dist/src/main/java/net/cosmogrp/storage/redis/CachedRedisModelService.java
@@ -2,7 +2,7 @@ package net.cosmogrp.storage.redis;
 
 import com.google.gson.Gson;
 import net.cosmogrp.storage.ModelService;
-import net.cosmogrp.storage.dist.RemoteModelService;
+import net.cosmogrp.storage.dist.CachedRemoteModelService;
 import net.cosmogrp.storage.model.Model;
 import net.cosmogrp.storage.model.meta.ModelMeta;
 import net.cosmogrp.storage.redis.connection.RedisCache;
@@ -11,18 +11,18 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 import java.util.concurrent.Executor;
 
-public class RedisModelService<T extends Model>
-        extends RemoteModelService<T> {
+public class CachedRedisModelService<T extends Model>
+        extends CachedRemoteModelService<T> {
     private final AbstractRedisModelService<T> delegate;
 
-    public RedisModelService(
+    public CachedRedisModelService(
             Executor executor,
             ModelMeta<T> modelMeta,
             Gson gson,
             ModelService<T> cacheModelService,
             RedisCache redisCache
     ) {
-        super(executor);
+        super(executor, cacheModelService);
         delegate = new AbstractRedisModelService<>(
                 executor, modelMeta, gson, redisCache
         );
