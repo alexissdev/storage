e105bc6d25ff2edafbaac4ed94bcd1cb675b0c42
pixeldev <pixeldev1729@gmail.com>
2022-02-07 20:05:42 -0600

    refactor(mongo-legacy-dist): use model meta to get collection name

--- mongo-legacy-dist/src/main/java/net/cosmogrp/storage/mongo/MongoModelService.java
+++ mongo-legacy-dist/src/main/java/net/cosmogrp/storage/mongo/MongoModelService.java
@@ -1,10 +1,12 @@
 package net.cosmogrp.storage.mongo;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.ReplaceOptions;
 import net.cosmogrp.storage.ModelService;
 import net.cosmogrp.storage.dist.CachedRemoteModelService;
+import net.cosmogrp.storage.model.meta.ModelMeta;
 import org.bson.Document;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,12 +22,20 @@ public class MongoModelService<T extends MongoModel>
 
     public MongoModelService(
             Executor executor,
+            ModelMeta<T> modelMeta,
             ModelService<T> cacheModelService,
-            MongoCollection<Document> mongoCollection,
+            MongoDatabase database,
             MongoModelParser<T> mongoModelParser
     ) {
         super(executor, cacheModelService);
-        this.mongoCollection = mongoCollection;
+
+        String collectionName = (String) modelMeta.getProperty("collection");
+
+        if (collectionName == null) {
+            throw new IllegalArgumentException("Collection name is not defined");
+        }
+
+        this.mongoCollection = database.getCollection(collectionName);
         this.mongoModelParser = mongoModelParser;
     }
 
