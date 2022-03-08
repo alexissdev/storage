a59ed02df49f5c6f9f2b7e533968a632b945853b
pixeldev <pixeldev1729@gmail.com>
2022-03-08 11:10:54 -0600

    test: prepare test environment

---
+++ mongo-legacy-dist/src/test/java/net/cosmogrp/storage/mongo/TestHelper.java
@@ -0,0 +1,46 @@
+package net.cosmogrp.storage.mongo;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import net.cosmogrp.storage.dist.CachedRemoteModelService;
+import net.cosmogrp.storage.dist.LocalModelService;
+import net.cosmogrp.storage.model.meta.ModelMeta;
+import net.cosmogrp.storage.mongo.model.DummyModel;
+import net.cosmogrp.storage.resolve.RelationalResolver;
+import net.cosmogrp.storage.resolve.ResolverRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+public final class TestHelper {
+
+    private TestHelper() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static CachedRemoteModelService<DummyModel> create() {
+        MongoClient client = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase database = client.getDatabase("admin");
+
+        Map<String, RelationalResolver<DummyModel>> resolvers =
+                new HashMap<>();
+
+        resolvers.put("someValue", new RelationalResolver<>(DummyModel::getSomeValue));
+
+        ResolverRegistry<DummyModel> resolverRegistry =
+                new ResolverRegistry<>(resolvers);
+
+        return new MongoModelService<>(
+                Executors.newSingleThreadExecutor(),
+                new ModelMeta<>(DummyModel.class)
+                        .addProperty("resolvers", resolverRegistry)
+                        .addProperty("collection", "test"),
+                new LocalModelService<>(),
+                database,
+                DummyModel::fromDocument
+        );
+    }
+
+}
