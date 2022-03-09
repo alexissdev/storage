d9979b20c6855f245113d1bd5a603197322d8564
pixeldev <pixeldev1729@gmail.com>
2022-03-09 15:11:44 -0600

    build: create spigot conventions plugin

---
+++ buildSrc/src/main/kotlin/storage.spigot-conventions.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("storage.common-conventions")
+}
+
+repositories {
+    maven("https://repo.codemc.io/repository/nms/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
