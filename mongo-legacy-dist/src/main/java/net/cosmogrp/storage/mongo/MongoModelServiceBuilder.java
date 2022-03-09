c9c403de5d1d90a3d901406db8483ec89fdf8619
pixeldev <pixeldev1729@gmail.com>
2022-03-09 12:00:08 -0600

    feat(mongo-legacy-dist): create mongo model service builder

---
+++ mongo-legacy-dist/src/main/java/net/cosmogrp/storage/mongo/MongoModelServiceBuilder.java
@@ -0,0 +1,57 @@
+package net.cosmogrp.storage.mongo;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import net.cosmogrp.storage.ModelService;
+import net.cosmogrp.storage.builder.LayoutModelServiceBuilder;
+import net.cosmogrp.storage.model.Model;
+import net.cosmogrp.storage.mongo.codec.DocumentCodec;
+import net.cosmogrp.storage.mongo.codec.MongoModelParser;
+import org.bson.Document;
+
+public class MongoModelServiceBuilder<T extends Model & DocumentCodec>
+        extends LayoutModelServiceBuilder<T, MongoModelServiceBuilder<T>> {
+
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+    private MongoModelParser<T> modelParser;
+
+    protected MongoModelServiceBuilder() {
+
+    }
+
+    public MongoModelServiceBuilder<T> database(MongoDatabase database) {
+        this.database = database;
+        return this;
+    }
+
+    public MongoModelServiceBuilder<T> modelParser(MongoModelParser<T> modelParser) {
+        this.modelParser = modelParser;
+        return this;
+    }
+
+    public MongoModelServiceBuilder<T> collection(String collection) {
+        this.collection = database.getCollection(collection);
+        return this;
+    }
+
+    @Override
+    public ModelService<T> build() {
+        check();
+
+        if (cacheModelService == null) {
+            return new MongoModelService<>(executor, collection, modelParser);
+        } else {
+            return new CachedMongoModelService<>(
+                    executor, cacheModelService,
+                    resolverRegistry, collection,
+                    modelParser
+            );
+        }
+    }
+
+    @Override
+    protected MongoModelServiceBuilder<T> back() {
+        return this;
+    }
+}
