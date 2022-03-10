524f1d16fbd4eb36ed11bc8f10b62e1571f427e0
pixeldev <pixeldev1729@gmail.com>
2022-03-10 07:57:47 -0600

    refactor(api-codec): model parser should receive model reader

--- api-codec/src/main/java/net/cosmogrp/storage/codec/ModelParser.java
+++ api-codec/src/main/java/net/cosmogrp/storage/codec/ModelParser.java
@@ -2,8 +2,8 @@ package net.cosmogrp.storage.codec;
 
 import net.cosmogrp.storage.model.Model;
 
-public interface ModelParser<T extends Model> {
+public interface ModelParser<T extends Model, R> {
 
-    T parse();
+    T parse(ModelReader<R> reader);
 
 }
