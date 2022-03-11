b6cea53888d7a2c7a67af7e0bfabf127e77b97a1
pixeldev <pixeldev1729@gmail.com>
2022-03-11 14:06:10 -0600

    refactor(bukkit-yaml): it couldn't be a ConfigurationSection

--- bukkit-yaml-dist/src/main/java/net/cosmogrp/storage/bukkit/codec/YamlModelParser.java
+++ bukkit-yaml-dist/src/main/java/net/cosmogrp/storage/bukkit/codec/YamlModelParser.java
@@ -2,9 +2,10 @@ package net.cosmogrp.storage.bukkit.codec;
 
 import net.cosmogrp.storage.codec.ModelParser;
 import net.cosmogrp.storage.model.Model;
-import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.Map;
 
 public interface YamlModelParser<T extends Model>
-        extends ModelParser<T, ConfigurationSection> {
+        extends ModelParser<T, Map<String, Object>> {
 
 }
