a88cf0302978ec9ae28776e4aa376b4104436654
pixeldev <pixeldev1729@gmail.com>
2022-03-28 18:08:25 -0600

    fix(bukkit-yaml): JODER BUENAS TARDES

--- bukkit-yaml-dist/src/main/java/net/cosmogrp/storage/bukkit/codec/YamlWriter.java
+++ bukkit-yaml-dist/src/main/java/net/cosmogrp/storage/bukkit/codec/YamlWriter.java
@@ -1,10 +1,13 @@
 package net.cosmogrp.storage.bukkit.codec;
 
+import net.cosmogrp.storage.codec.ModelCodec;
 import net.cosmogrp.storage.codec.ModelWriter;
 import net.cosmogrp.storage.codec.DelegateObjectModelWriter;
 import net.cosmogrp.storage.model.Model;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class YamlWriter extends DelegateObjectModelWriter<Map<String, Object>> {
@@ -22,6 +25,25 @@ public class YamlWriter extends DelegateObjectModelWriter<Map<String, Object>> {
     public static ModelWriter<Map<String, Object>> create(Model model) {
         return new YamlWriter()
                 .write("id", model.getId());
+    }
+
+    @Override
+    public ModelWriter<Map<String, Object>> write(
+            String field,
+            Collection<? extends ModelCodec<Map<String, Object>>> children
+    ) {
+        Map<String, Object> values = new HashMap<>();
+        Iterator<? extends ModelCodec<Map<String, Object>>> iterator
+                = children.iterator();
+
+        int i = 0;
+        while (iterator.hasNext()) {
+            ModelCodec<Map<String, Object>> codec = iterator.next();
+            values.put(String.valueOf(i++), codec.serialize());
+        }
+
+        this.values.put(field, values);
+        return this;
     }
 
     @Override
